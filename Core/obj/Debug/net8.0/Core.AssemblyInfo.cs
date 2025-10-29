db14e04768ae6133bc375162fa8f1f9a764eed40
fa1ry <progamerdanila@gmail.com>
2025-10-30 02:48:01 +0300

    n

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb30556855cd64d75523125b63134204d4187de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d995524fa91533e692cf2f5817e640f6da724e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
